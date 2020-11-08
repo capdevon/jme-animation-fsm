8defabe5e5ad85522ac259c503a7d441d1253ca2
Wyatt Gillette <jefferydeaver2010@gmail.com>
2020-11-08 18:26:29 +0100

    Add files via upload

---
+++ src/main/java/com/capdevon/physx/RaycastHit.java
@@ -0,0 +1,42 @@
+package com.capdevon.physx;
+
+import com.jme3.bullet.collision.PhysicsCollisionObject;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.math.Vector3f;
+
+public class RaycastHit {
+
+	public PhysicsCollisionObject rigidbody;
+	public CollisionShape collider;
+    public Object userObject;
+    public float distance;
+    public Vector3f normal = new Vector3f();
+    public Vector3f point = new Vector3f();
+    
+    public void clear() {
+        rigidbody = null;
+        collider = null;
+        userObject = null;
+        distance = Float.NaN;
+        point.set(Vector3f.NAN);
+        normal.set(Vector3f.NAN);
+    }
+    
+	@Override
+	public String toString() {
+		return "RaycastHit [rigidbody=" + toHexString(rigidbody) 
+				+ ", collider=" + toHexString(collider)
+				+ ", userObject=" + toHexString(userObject)
+				+ ", distance=" + distance 
+				+ ", normal=" + normal 
+				+ ", point=" + point 
+				+ "]";
+	}
+	
+	private String toHexString(Object obj) {
+		if (obj != null)
+			return obj.getClass().getSimpleName() + '@' + Integer.toHexString(obj.hashCode());
+		return null;
+	}
+    
+}
