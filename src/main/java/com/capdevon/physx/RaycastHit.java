9faee1b11698c006562884d097f3619d480c3e2c
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-03-28 12:08:35 +0200

    Update RaycastHit.java

--- src/main/java/com/capdevon/physx/RaycastHit.java
+++ src/main/java/com/capdevon/physx/RaycastHit.java
@@ -6,37 +6,37 @@ import com.jme3.math.Vector3f;
 
 public class RaycastHit {
 
-	public PhysicsCollisionObject rigidBody;
-	public CollisionShape collider;
+    public PhysicsCollisionObject rigidBody;
+    public CollisionShape collider;
     public Object gameObject;
     public float distance;
     public Vector3f normal = new Vector3f();
     public Vector3f point = new Vector3f();
-    
+
     public void clear() {
-    	rigidBody = null;
+        rigidBody = null;
         collider = null;
         gameObject = null;
         distance = Float.NaN;
         point.set(Vector3f.NAN);
         normal.set(Vector3f.NAN);
     }
-    
-	@Override
-	public String toString() {
-		return "RaycastHit [rigidbody=" + toHexString(rigidBody) 
-				+ ", collider=" + toHexString(collider)
-				+ ", gameObject=" + toHexString(gameObject)
-				+ ", distance=" + distance 
-				+ ", normal=" + normal 
-				+ ", point=" + point 
-				+ "]";
-	}
-	
-	private String toHexString(Object obj) {
-		if (obj != null)
-			return obj.getClass().getSimpleName() + '@' + Integer.toHexString(obj.hashCode());
-		return null;
-	}
-    
+
+    @Override
+    public String toString() {
+        return "RaycastHit [rigidbody=" + toHexString(rigidBody) +
+            ", collider=" + toHexString(collider) +
+            ", gameObject=" + toHexString(gameObject) +
+            ", distance=" + distance +
+            ", normal=" + normal +
+            ", point=" + point +
+            "]";
+    }
+
+    private String toHexString(Object obj) {
+        if (obj != null)
+            return obj.getClass().getSimpleName() + '@' + Integer.toHexString(obj.hashCode());
+        return null;
+    }
+
 }
