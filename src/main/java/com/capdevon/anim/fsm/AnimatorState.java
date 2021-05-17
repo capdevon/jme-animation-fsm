c76f1c1e3551d3566a89ab6fca3e57d33547b83a
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-05-17 09:36:08 +0200

    add new feature StateMachineBehaviour

--- src/main/java/com/capdevon/anim/fsm/AnimatorState.java
+++ src/main/java/com/capdevon/anim/fsm/AnimatorState.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 /**
  * States are the basic building blocks of a state machine. Each state contains
- * a Motion (AnimationClip or BlendTree) which will play while the character is
+ * a Motion (AnimClip or BlendTree) which will play while the character is
  * in that state. When an event in the game triggers a state transition, the
  * character will be left in a new state whose animation sequence will then take
  * over.
@@ -20,28 +20,38 @@ import java.util.List;
  */
 public class AnimatorState {
 
-    private final AnimatorController animator;
+    private AnimatorController animator;
     
     //The animation clip assigned to this state
     protected String action;
     //A name can be used to identify a state.
     protected String name;
     //The default speed of the motion.
-    public float speed = 1f;
+    protected float speed = 1f;
     //The transitions that are going out of the state.
     protected List<AnimatorStateTransition> transitions = new ArrayList<>();
-    
+    //The behaviour list assigned to this state.
+    protected List<StateMachineBehaviour> behaviours = new ArrayList<>();
+        
     /**
-     * protected constructor.
+     * Constructor.
      * @param animator
      */
-    protected AnimatorState(AnimatorController animator) {
+    protected AnimatorState(String name, AnimatorController animator) {
+    	this.name = name;
         this.animator = animator;
     }
     
     /**
+     * Adds a state machine behaviour class to the AnimatorState.
+     * @param behaviour The state machine behaviour to add.
+     */
+    public void addStateMachineBehaviour(StateMachineBehaviour behaviour) {
+    	behaviours.add(behaviour);
+    }
+    
+    /**
      * Utility function to remove a transition from the state.
-     * 
      * @param transition Transition to remove.
      */
     public void removeTransition(AnimatorStateTransition transition) {
@@ -50,7 +60,6 @@ public class AnimatorState {
     
     /**
      * Utility function to add an outgoing transition to the destination state.
-     * 
      * @param destinationState The destination state.
      * @return 
      */
@@ -80,10 +89,25 @@ public class AnimatorState {
         return this;
     }
     
-    private double getAnimPercent() {
-        return animator.animComposer.getTime() / animator.animComposer.getAnimClip(action).getLength();
-    }
-
+	private double getAnimPercent() {
+		if (animator != null) {
+			return animator.animComposer.getTime() / animator.animComposer.getAnimClip(action).getLength();
+		}
+		return 0;
+	}
+	
+	public String getAction() {
+		return action;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public float getSpeed() {
+		return speed;
+	}
+    
     @Override
     public String toString() {
         return "AnimatorState{" 
