5fdb7f7af3fe4c265b85b3aea5f72b4e5b2a16aa
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-05-19 09:18:43 +0200

    format code

--- src/main/java/com/capdevon/anim/fsm/StateMachineListener.java
+++ src/main/java/com/capdevon/anim/fsm/StateMachineListener.java
@@ -5,12 +5,12 @@ package com.capdevon.anim.fsm;
  * @author capdevon
  */
 public interface StateMachineListener {
-	
-	/**
-	 * These listeners will be invoked any time a state transition occurs and can be utilized for things such as logging or other.
-	 * @param from
-	 * @param to
-	 */
-	public void onStateChanged(AnimatorState from, AnimatorState to);
+
+    /**
+     * These listeners will be invoked any time a state transition occurs and can be utilized for things such as logging or other.
+     * @param from
+     * @param to
+     */
+    public void onStateChanged(AnimatorState from, AnimatorState to);
 
 }
