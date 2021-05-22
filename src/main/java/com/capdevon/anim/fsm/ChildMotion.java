8df9a6dbdf3b0f67f9c86c277977fdd511371b9d
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-05-22 10:36:57 +0200

    format code and added comments

--- src/main/java/com/capdevon/anim/fsm/ChildMotion.java
+++ src/main/java/com/capdevon/anim/fsm/ChildMotion.java
@@ -14,22 +14,22 @@ import com.jme3.math.Vector2f;
  */
 public class ChildMotion {
 
-	//The relative speed of the child.
+    //The relative speed of the child.
     public float timeScale = 1f;
     //The motion itself.
     public String animName;
-    //The position of the child. Used in 2D blend trees.
+    //The position of the child. Used in 2D blend trees. (not yet supported)
     public Vector2f position;
     //The threshold of the child. Used in 1D blend trees.
     public float threshold;
-    //Normalized time offset of the child.
+    //Normalized time offset of the child. (not yet supported)
     public float cycleOffset;
-    //The parameter used by the child when used in a BlendTree of type BlendTreeType.Direct.
+    //The parameter used by the child when used in a BlendTree of type BlendTreeType.Direct. (not yet supported)
     public String directBlendParameter = "Blend";
-    
-	@Override
-	public String toString() {
-		return "ChildMotion [timeScale=" + timeScale + ", animName=" + animName + ", threshold=" + threshold + "]";
-	}
-    
+
+    @Override
+    public String toString() {
+        return "ChildMotion [timeScale=" + timeScale + ", animName=" + animName + ", threshold=" + threshold + "]";
+    }
+
 }
