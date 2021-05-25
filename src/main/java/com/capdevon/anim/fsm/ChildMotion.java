2e594bfedc950fb446ad16760dbe0edb8442f41b
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-05-25 16:56:28 +0200

    protected fields

--- src/main/java/com/capdevon/anim/fsm/ChildMotion.java
+++ src/main/java/com/capdevon/anim/fsm/ChildMotion.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.capdevon.anim.fsm;
 
 import com.jme3.math.Vector2f;
@@ -15,18 +10,39 @@ import com.jme3.math.Vector2f;
 public class ChildMotion {
 
     //The relative speed of the child.
-    public float timeScale = 1f;
+    protected float timeScale = 1f;
     //The motion itself.
-    public String animName;
+    protected String animName;
     //The position of the child. Used in 2D blend trees. (not yet supported)
-    public Vector2f position;
+    protected Vector2f position;
     //The threshold of the child. Used in 1D blend trees.
-    public float threshold;
-    //Normalized time offset of the child. (not yet supported)
-    public float cycleOffset;
+    protected float threshold;
+    //Normalized time offset of the child.
+    protected float cycleOffset;
     //The parameter used by the child when used in a BlendTree of type BlendTreeType.Direct. (not yet supported)
-    public String directBlendParameter = "Blend";
+    protected String directBlendParameter = "Blend";
 
+    
+    public String getAnimName() {
+		return animName;
+	}
+
+	public float getTimeScale() {
+		return timeScale;
+	}
+
+	public void setTimeScale(float timeScale) {
+		this.timeScale = timeScale;
+	}
+
+	public float getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(float threshold) {
+		this.threshold = threshold;
+	}
+	
     @Override
     public String toString() {
         return "ChildMotion [timeScale=" + timeScale + ", animName=" + animName + ", threshold=" + threshold + "]";
