da8d24aa675c84ff9835466e19bf222cabdc301f
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-06-04 15:03:50 +0200

    new feature BlendTree 2D

--- src/main/java/com/capdevon/anim/fsm/BlendTree.java
+++ src/main/java/com/capdevon/anim/fsm/BlendTree.java
@@ -15,10 +15,10 @@ public class BlendTree extends Motion {
 
     public enum BlendTreeType {
         Simple1D,
-//        SimpleDirectional2D,      //not yet supported
-//        FreeformDirectional2D,    //not yet supported
-//        FreeformCartesian2D,      //not yet supported
-//        Direct                    //not yet supported
+        SimpleDirectional2D,
+//        FreeformDirectional2D,
+//        FreeformCartesian2D,
+//        Direct
     }
 
     //Parameter that is used to compute the blending weight of the childs in 1D blend trees or on the X axis of a 2D blend tree.
@@ -33,6 +33,13 @@ public class BlendTree extends Motion {
     protected float minThreshold = 0f;
     //The list of the blend tree child motions.
     protected List<ChildMotion> motions = new ArrayList<>();
+    
+    /**
+     * Constructor.
+     */
+    public BlendTree() {
+    	//default empty.
+    }
 
     /**
      * Create blend tree with the minimum and maximum threshold for the LinearBlendSpace.
@@ -44,7 +51,7 @@ public class BlendTree extends Motion {
         this.minThreshold = minThreshold;
         this.maxThreshold = maxThreshold;
     }
-
+    
     /**
      * Utility function to remove the child of a blend tree.
      * 
@@ -59,6 +66,7 @@ public class BlendTree extends Motion {
      * 
      * @param animName - The motion to add as child.
      * @param position - The position of the child. When using 2D blend trees.
+     * @return 
      */
     public ChildMotion addChild(String animName, Vector2f position) {
         return addChild(animName, position, 0f);
@@ -69,6 +77,7 @@ public class BlendTree extends Motion {
      * 
      * @param animName  - The motion to add as child.
      * @param threshold - The threshold of the child. When using 1D blend trees.
+     * @return 
      */
     public ChildMotion addChild(String animName, float threshold) {
         return addChild(animName, Vector2f.ZERO.clone(), threshold);
@@ -92,12 +101,28 @@ public class BlendTree extends Motion {
         return clips;
     }
 
+    public BlendTreeType getBlendType() {
+        return blendType;
+    }
+
+    public void setBlendType(BlendTreeType blendType) {
+        this.blendType = blendType;
+    }
+
     public String getBlendParameter() {
         return blendParameter;
     }
 
     public void setBlendParameter(String blendParameter) {
         this.blendParameter = blendParameter;
+    }
+
+    public String getBlendParameterY() {
+        return blendParameterY;
+    }
+
+    public void setBlendParameterY(String blendParameterY) {
+        this.blendParameterY = blendParameterY;
     }
 
     public float getMaxThreshold() {
@@ -107,5 +132,101 @@ public class BlendTree extends Motion {
     public float getMinThreshold() {
         return minThreshold;
     }
+    
+    /**
+     * InternalCall.
+     * @param blendPos
+     * @return
+     */
+    protected ChildMotion getBlendMotion(Vector2f blendPos) {
+        ChildMotion closestChild = null;
+        float minDistance = Float.MAX_VALUE;
 
+        for (ChildMotion childMotion: motions) {
+            float d = childMotion.position.distanceSquared(blendPos);
+            if (d < minDistance) {
+                minDistance = d;
+                closestChild = childMotion;
+            }
+        }
+
+        return closestChild;
+    }
+
+    /**
+     * InternalCall.
+     * @param blendPos
+     * @return
+     */
+    protected ChildMotion getBlendMotion(float blendPos) {
+
+        int point_lower = -1;
+        float pos_lower = 0.0f;
+        int point_higher = -1;
+        float pos_higher = 0.0f;
+        float[] weights = new float[motions.size()];
+
+        for (int i = 0; i < motions.size(); i++) {
+            float pos = motions.get(i).threshold;
+
+            if (pos <= blendPos) {
+                if (point_lower == -1) {
+                    point_lower = i;
+                    pos_lower = pos;
+                } else if ((blendPos - pos) < (blendPos - pos_lower)) {
+                    point_lower = i;
+                    pos_lower = pos;
+                }
+            } else {
+                if (point_higher == -1) {
+                    point_higher = i;
+                    pos_higher = pos;
+                } else if ((pos - blendPos) < (pos_higher - blendPos)) {
+                    point_higher = i;
+                    pos_higher = pos;
+                }
+            }
+        }
+
+        if (point_lower == -1 && point_higher != -1) {
+            // we are on the left side, no other point to the left
+            // we just play the next point.
+            weights[point_higher] = 1.0f;
+
+        } else if (point_higher == -1) {
+            // we are on the right side, no other point to the right
+            // we just play the previous point
+            weights[point_lower] = 1.0f;
+
+        } else {
+            // we are between two points.
+            // figure out weights, then blend the animations
+            float distance_between_points = pos_higher - pos_lower;
+            float current_pos_inbetween = blendPos - pos_lower;
+            float blend_percentage = current_pos_inbetween / distance_between_points;
+
+            float blend_lower = 1.0f - blend_percentage;
+            float blend_higher = blend_percentage;
+
+            weights[point_lower] = blend_lower;
+            weights[point_higher] = blend_higher;
+        }
+
+        return motions.get(getIndexOfMax(weights));
+    }
+
+    protected int getIndexOfMax(float[] array) {
+        if (array == null || array.length == 0)
+            return -1; // null or empty
+
+        int max = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[max])
+                max = i;
+        }
+
+        // position of the first largest found
+        return max;
+    }
+    
 }
