f13d6be123d7e7ff553e1030025cc8e4a4593c1f
Wyatt Gillette <jefferydeaver2010@gmail.com>
2021-06-14 09:49:48 +0200

    format code

--- src/main/java/com/capdevon/anim/ActionAnimEventListener.java
+++ src/main/java/com/capdevon/anim/ActionAnimEventListener.java
@@ -2,9 +2,13 @@ package com.capdevon.anim;
 
 import com.jme3.anim.AnimComposer;
 
+/**
+ * 
+ * @author capdevon
+ */
 public interface ActionAnimEventListener {
 
-	public void onAnimCycleDone(CustomAction action, AnimComposer animComposer, String animName);
+    public void onAnimCycleDone(CustomAction action, AnimComposer animComposer, String animName);
 
-	public void onAnimChange(CustomAction action, AnimComposer animComposer, String animName);
+    public void onAnimChange(CustomAction action, AnimComposer animComposer, String animName);
 }
