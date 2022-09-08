bd5459d75d416edcfb25a32c033f9755b77c7c60
Wyatt Gillette <jefferydeaver2010@gmail.com>
2022-09-08 15:20:04 +0200

    optimization

--- src/main/java/com/capdevon/physx/RaycastHit.java
+++ src/main/java/com/capdevon/physx/RaycastHit.java
@@ -1,11 +1,15 @@
 package com.capdevon.physx;
 
 import com.jme3.bullet.collision.PhysicsCollisionObject;
+import com.jme3.bullet.collision.PhysicsRayTestResult;
+import com.jme3.bullet.collision.PhysicsSweepTestResult;
 import com.jme3.bullet.collision.shapes.CollisionShape;
 import com.jme3.math.Vector3f;
 
+import jme3utilities.math.MyVector3f;
+
 /**
- *
+ * 
  * @author capdevon
  */
 public class RaycastHit {
@@ -17,6 +21,29 @@ public class RaycastHit {
     public Vector3f normal = new Vector3f();
     public Vector3f point = new Vector3f();
 
+    protected void set(Vector3f beginVec, Vector3f finalVec, PhysicsRayTestResult ray) {
+        PhysicsCollisionObject pco = ray.getCollisionObject();
+        float hf = ray.getHitFraction();
+
+        rigidBody = pco;
+        collider = pco.getCollisionShape();
+        gameObject = pco.getUserObject();
+        distance = finalVec.subtract(beginVec).length() * hf;
+        point.interpolateLocal(beginVec, finalVec, hf);
+        ray.getHitNormalLocal(normal);
+    }
+    
+    protected void set(Vector3f beginVec, Vector3f finalVec, PhysicsSweepTestResult tr) {
+        PhysicsCollisionObject pco = tr.getCollisionObject();
+
+        rigidBody = pco;
+        collider = pco.getCollisionShape();
+        gameObject = pco.getUserObject();
+        MyVector3f.lerp(tr.getHitFraction(), beginVec, finalVec, point);
+        tr.getHitNormalLocal(normal);
+        distance = beginVec.distance(point);
+    }
+    
     public void clear() {
         rigidBody = null;
         collider = null;
@@ -28,19 +55,18 @@ public class RaycastHit {
 
     @Override
     public String toString() {
-        return "RaycastHit [rigidbody=" + toHexString(rigidBody)
-                + ", collider=" + toHexString(collider)
-                + ", gameObject=" + toHexString(gameObject)
-                + ", distance=" + distance
-                + ", normal=" + normal
-                + ", point=" + point
-                + "]";
+        return "RaycastHit [rigidbody=" + toHexString(rigidBody) +
+            ", collider=" + toHexString(collider) +
+            ", gameObject=" + toHexString(gameObject) +
+            ", distance=" + distance +
+            ", normal=" + normal +
+            ", point=" + point +
+            "]";
     }
 
     private String toHexString(Object obj) {
-        if (obj != null) {
+        if (obj != null)
             return obj.getClass().getSimpleName() + '@' + Integer.toHexString(obj.hashCode());
-        }
         return null;
     }
 
