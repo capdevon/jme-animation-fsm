152de96f345ba3d957cb22ee408e582c48371a01
capdevon <jefferydeaver2010@gmail.com>
2024-03-07 10:45:45 +0100

    new AnimDefs interface

---
+++ src/main/java/com/capdevon/demo/AnimDefs.java
@@ -0,0 +1,19 @@
+package com.capdevon.demo;
+
+public interface AnimDefs {
+
+    final String MODEL = "Models/Rifle/rifle.glb";
+    final String RifleIdle = "RifleIdle";
+    final String RifleWalk = "RifleWalk";
+    final String RifleRun = "RifleRun";
+    final String WalkWithRifle = "WalkWithRifle";
+    final String ThrowGrenade = "ThrowGrenade";
+    final String Reloading = "Reloading";
+    final String RifleAimingIdle = "RifleAimingIdle";
+    final String FiringRifleSingle = "FiringRifleSingle";
+    final String FiringRifleAuto = "FiringRifleAuto";
+    final String DeathFromRight = "DeathFromRight";
+    final String DeathFromHeadshot = "DeathFromHeadshot";
+    final String TPose = "TPose";
+
+}
