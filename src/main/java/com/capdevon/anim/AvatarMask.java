94c93342a6003a6a6cf1a075da43d9185c70e5f3
capdevon <jefferydeaver2010@gmail.com>
2024-09-17 18:26:50 +0200

    AvatarMask: implements Savable interface

--- src/main/java/com/capdevon/anim/AvatarMask.java
+++ src/main/java/com/capdevon/anim/AvatarMask.java
@@ -1,17 +1,23 @@
 package com.capdevon.anim;
 
+import java.io.IOException;
 import java.util.BitSet;
 
 import com.jme3.anim.AnimationMask;
 import com.jme3.anim.Joint;
+import com.jme3.export.InputCapsule;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.export.OutputCapsule;
+import com.jme3.export.Savable;
 
 /**
  *
  * @author capdevon
  */
-public class AvatarMask implements AnimationMask {
+public class AvatarMask implements AnimationMask, Savable {
 
-    private final BitSet affectedJoints = new BitSet();
+    private BitSet affectedJoints = new BitSet();
 
     /**
      * Instantiate a mask that affects no joints.
@@ -32,6 +38,18 @@ public class AvatarMask implements AnimationMask {
     public boolean contains(Object target) {
         Joint joint = (Joint) target;
         return affectedJoints.get(joint.getId());
+    }
+
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        OutputCapsule oc = ex.getCapsule(this);
+        oc.write(affectedJoints, "affectedJoints", null);
+    }
+
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        InputCapsule ic = im.getCapsule(this);
+        affectedJoints = ic.readBitSet("affectedJoints", null);
     }
 
 }
